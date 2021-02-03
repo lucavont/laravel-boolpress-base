babb633cd2cb6aa6815a6d151e541da0d20db12d
Luca Cavretti <cavretiluca@live.it>
2021-02-03 10:45:59 +0100

    Exercise done same as Flavio

---
+++ database/seeds/PostInformationSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostInformationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\PostInformation::class, 100)->create();
+    }
+}
